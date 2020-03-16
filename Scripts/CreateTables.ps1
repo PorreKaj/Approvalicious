e7999c7ecb2dbc8f8b4e2d0be047991008030158
Rene Hansen <HansenRene@outlook.com>
2020-03-16 07:23:00 +0100

    Create CreateTables.ps1

---
+++ Scripts/CreateTables.ps1
@@ -0,0 +1,35 @@
+﻿#Requires -Modules invoke-SQLCMD2
+
+$Server = "CIMa\CIMa"
+$Database = "CIMa_Approvals"
+
+$CreateMainTableQuery = "
+
+CREATE TABLE ApprovalRequests (
+    ApprovalID int IDENTITY(1,1) PRIMARY KEY,
+    ApprovalGUID UNIQUEIDENTIFIER DEFAULT NEWSEQUENTIALID(),
+    
+    RequestStatus varchar(255),
+    RequestDate datetime,
+    RequestProduct varchar(255),
+    RequestResult varchar(255),
+    RequestResultDate datetime,
+    RequestRestultExpirationDate datetime,
+
+    Source varchar(255),
+    SourceID varchar(255),
+    
+    RequesteeUsername varchar(255),
+    RequesteeObjectGUID varchar(255),
+    RequesteeComment varchar(max),
+
+    ApproverUsername varchar(255),
+    ApproverObjectGUID varchar(255),
+    ApproverComment varchar(max)
+    
+);
+
+
+"
+
+Invoke-Sqlcmd2 -ServerInstance $Server -Database $Database -Query $CreateMainTableQuery
